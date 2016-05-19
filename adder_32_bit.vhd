66a2d30f5075412c56665fd08035ac7ad9da99d1
Jay Valentine <jayv136@gmail.com>
2016-05-19 15:31:53 +0100

    Changed ALU adder to unsigned arithmetic
    ALU adder now performs unsigned arithmetic - will add another control
    signal to select between signed and unsigned

--- adder_32_bit.vhd
+++ adder_32_bit.vhd
@@ -1,5 +1,5 @@
 -- 32-bit adder circuit
--- this circuit adds two 32-bit numbers together, and supports previous-carry addition
+-- this circuit adds two 32-bit unsigned numbers together, and supports previous-carry addition and subtraction
 -- all code (c) 2016 Jay Valentine, released under the MIT license
 
 library IEEE;
@@ -14,6 +14,7 @@ entity adder_32_bit is
 		c_in		:	in		std_logic;
 		opcode	:	in		std_logic_vector(1 downto 0);
 		enable	:	in		std_logic;
+		sign		:	in		std_logic;
 		
 		-- outputs
 		sum_32	:	out	std_logic_vector(31 downto 0);
@@ -24,10 +25,10 @@ end entity adder_32_bit;
 
 architecture adder_32_bit_arch of adder_32_bit is
 	-- defining signals
-	signal a_numeric		:	signed(32 downto 0);
-	signal b_numeric		:	signed(32 downto 0);
-	signal c_numeric		:	signed(32 downto 0);
-	signal tmp_sum			:	signed(32 downto 0);
+	signal a_numeric		:	unsigned(32 downto 0);
+	signal b_numeric		:	unsigned(32 downto 0);
+	signal c_numeric		:	unsigned(32 downto 0);
+	signal tmp_sum			:	unsigned(32 downto 0);
 	
 begin
 	-- design implementation
@@ -35,8 +36,8 @@ begin
 	begin
 		if enable = '1' then
 			-- converting inputs to unsigned vectors
-			a_numeric <= '0' & signed(a_32);
-			b_numeric <= '0' & signed(b_32);
+			a_numeric <= '0' & unsigned(a_32);
+			b_numeric <= '0' & unsigned(b_32);
 			
 			-- converting previous carry so that it can be added to a and b
 			c_numeric <= (0 => c_in, others => '0');
